818d94264ee82b00adfd4759867b157dfea5ba5a
kaka0913 <kabuyuu0913@gmail.com>
2025-07-27 13:15:14 +0900

    Initial Commit

---
+++ Team8App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team8App
+//
+//  Created by 株丹優一郎 on 2025/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team8App/Team8AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team8AppApp.swift
+//  Team8App
+//
+//  Created by 株丹優一郎 on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Team8AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
